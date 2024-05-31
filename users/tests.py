a9840449e3e77e587349e3b5bddf3396b3798326
Chiemeka Alim <lord.chiemy@gmail.com>
2024-05-31 21:24:37 +0200

    Update custom user/customer tests - use get_user_model() and add comments

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from rest_framework.reverse import reverse
 from rest_framework.test import APIClient
@@ -7,7 +8,8 @@ from users.models import Customer
 
 class CustomUserManagerTest(TestCase):
     def test_create_user(self):
-        user = Customer.objects.create_user(email="test@user.com", password="foo")
+        User = get_user_model()
+        user = User.objects.create_user(email="test@user.com", password="foo")
         self.assertEqual(user.email, "test@user.com")
         self.assertTrue(user.is_active)
         self.assertFalse(user.is_staff)
@@ -24,7 +26,8 @@ class CustomUserManagerTest(TestCase):
             Customer.objects.create_user(email="", password="foo")
 
     def test_create_superuser(self):
-        admin_user = Customer.objects.create_superuser(email="super@user.com", password="foo")
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(email="super@user.com", password="foo")
         self.assertEqual(admin_user.email, "super@user.com")
         self.assertTrue(admin_user.is_active)
         self.assertTrue(admin_user.is_staff)
@@ -42,19 +45,24 @@ class CustomerTest(TestCase):
     def setUp(self):
         self.client = APIClient()
 
+        # Create first customer
         url = reverse('customer-create')
-        self.client.post(url, {'email': 'customer1@user.com', 'password': '<PASSWORD>'})
+        self.client.post(url, {'email': 'customer1@user.com', 'password': 'foo'})
 
     def test_create_customer(self):
+        # Create second customer
         url = reverse('customer-create')
-        response = self.client.post(url, {'email': 'customer2@user.com', 'password': '<PASSWORD>'})
+        response = self.client.post(url, {'email': 'customer2@user.com', 'password': 'foo'})
+
         self.assertEqual(response.status_code, 201)
         self.assertEqual(Customer.objects.count(), 2)
         self.assertEqual(Customer.objects.last().email, "customer2@user.com")
 
     def test_retrieve_customer(self):
+        # Retrieve first customer
         url = reverse('customer-detail', kwargs={'pk': 1})
         response = self.client.get(url)
+
         self.assertEqual(response.status_code, 200)
         self.assertEqual(Customer.objects.count(), 1)
         self.assertEqual(Customer.objects.last().email, "customer1@user.com")
